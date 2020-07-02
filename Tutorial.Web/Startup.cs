e4c53afcde85b4421164d28c10003cb9af7294c9
ssss10000 <ssss10000@163.com>
2020-07-02 17:17:45 +0800

    数据库

--- Tutorial.Web/Startup.cs
+++ Tutorial.Web/Startup.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Tutorial.Web.Data;
 using Tutorial.Web.Model;
 using Tutorial.Web.Services;
 
@@ -10,13 +13,25 @@ namespace Tutorial.Web
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            var connectionString = configuration["ConnectionStrings:DefaultConnection"];
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
             services.AddSingleton<IWelcomeService, WelcomeServie>();
-            services.AddSingleton<IRepository<Student>, InMemoryRepository>();
+            services.AddScoped<IRepository<Student>, EFCoreRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,11 +77,12 @@ namespace Tutorial.Web
 
             app.UseMvcWithDefaultRoute();
 
-            app.Run(async (context) =>
-            {
-                var welcome = welcomeServie.getMessage();
-                await context.Response.WriteAsync(welcome);
-            });
+            //
+            //app.Run(async (context) =>
+            //{
+            //    var welcome = welcomeServie.getMessage();
+            //    await context.Response.WriteAsync(welcome);
+            //});
         }
     }
 }
