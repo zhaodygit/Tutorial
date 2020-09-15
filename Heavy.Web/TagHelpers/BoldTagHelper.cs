77398763e9d667ff53101c24a70285d12a51b2dd
ssss10000 <ssss10000@163.com>
2020-09-15 17:05:12 +0800

    tag helpers

---
+++ Heavy.Web/TagHelpers/BoldTagHelper.cs
@@ -0,0 +1,33 @@
+﻿using Heavy.Web.TagHelpers.Models;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Heavy.Web.TagHelpers
+{
+    /**
+     * [HtmlTargetElement(Attributes = "bold")]
+     * 或
+     *  [HtmlTargetElement("bold")]
+    */
+    [HtmlTargetElement(Attributes = "bold")]
+    [HtmlTargetElement("bold")]
+    public class BoldTagHelper : TagHelper
+    {
+        [HtmlAttributeName("color")]
+        public string MyColor { get; set; }
+
+        [HtmlAttributeName("my-style")]
+        public MyStyle MyStyle { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll("bold");
+            output.PreContent.SetHtmlContent("<strong>");
+            output.PostContent.SetHtmlContent("</strong>");
+            output.Attributes
+                .SetAttribute("style", $"color:{MyStyle.Color};font-size:{MyStyle.FontSize};font-family:{MyStyle.FontFamily};");
+        }
+    }
+}
---
+++ Heavy.Web/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Heavy.Web.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string MailTo { get; set; }
+        //public override void Process(TagHelperContext context, TagHelperOutput output)
+        //{
+        //    output.TagName = "a";
+        //    output.Attributes.SetAttribute("href", $"mailto:{MailTo}");
+        //    output.Content.SetContent(MailTo);
+        //    //base.Process(context, output);
+
+        //}
+
+        public async override Task ProcessAsync(
+            TagHelperContext context,
+            TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent();
+            output.Attributes.SetAttribute("href", "mailto:"+ target);
+            output.Content.SetContent(target);
+        }
+    }
+}
