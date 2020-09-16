ef3cc819fce8bad8681c2a292c44872602de197a
ssss10000 <ssss10000@163.com>
2020-09-16 14:32:19 +0800

    tag helper2

---
+++ Heavy.Web/ViewComponents/InternetStatus.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Heavy.Web.ViewComponents
+{
+    public class InternetStatus : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://www.baidu.com");
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                return View(true);
+            }
+            return View(false);
+        }
+    }
+}
